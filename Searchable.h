dc8b6a28ed6be0a34188d0255bc15365f85077b6
Adi Shani <adishani15@gmail.com>
2019-01-06 18:39:12 +0200

    adi changes

---
+++ Searchable.h
@@ -0,0 +1,12 @@
+//
+// Created by adi on 1/6/19.
+//
+
+#ifndef PART2_SEARCHABLE_H
+#define PART2_SEARCHABLE_H
+
+class Searchable{
+
+};
+
+#endif //PART2_SEARCHABLE_H
---
+++ Searcher.h
@@ -0,0 +1,17 @@
+//
+// Created by adi on 1/6/19.
+//
+
+#ifndef PART2_SEARCHER_H
+#define PART2_SEARCHER_H
+
+#include "Searchable.h"
+#include <string>
+using namespace std;
+
+class Searcher{
+   string search(Searchable *searchable);
+
+};
+
+#endif //PART2_SEARCHER_H
